51a5396acff9656374bba35db78eb104dde04129
Rebecca <rebecca.lee.barrow.scott@gmail.com>
2020-08-29 02:02:51 +1000

    login code from week 5 lab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { AllUserComponent } from './all-user/all-user.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'user', component: UserComponent},
-  {path: 'allUser', component: AllUserComponent}
+  {path: 'allUser', component: AllUserComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+import { NgForm } from '@angular/forms';
+import { UserPwd } from '../class/userpwd';
+import { UserObj } from '../class/userobj';
+// import { USERPWD } from '../mock-users';
+const BACKEND_URL = 'http://localhost:3000';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +17,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  email:string = ""
+  password:string = ""
+  verify:boolean = false
+  feedback:string = ""
+  user:UserObj = {username: "User1", birthdate: "01/01/1990", age: 30, email: "user1@gmail.com", password: "123", valid: false}
+  userpwd = new UserPwd()
 
-  constructor() { }
 
+  constructor(private router:Router, private httpClient:HttpClient) { }
+  
   ngOnInit(): void {
   }
 
+  public loginFunc(){
+    this.userpwd.email = this.email
+    this.userpwd.password = this.password
+
+    this.httpClient.post(BACKEND_URL + '/api/auth', this.userpwd, httpOptions)
+    .subscribe((data: any) => {
+      if (data.valid){
+        sessionStorage.setItem('username', data.username);
+        sessionStorage.setItem('birthdate', data.birthdate);
+        sessionStorage.setItem('age', data.age);
+        sessionStorage.setItem('email', data.email);
+        this.router.navigateByUrl('user');
+      } else {
+        alert('Sorry, email or password is wrong')
+      }
+    });
+  }
 }
